4a0c5a85ee380b339f547b6464373b8f59fd4a53
Diego Imbert <diegoimbert@pm.me>
2023-10-05 11:00:08 +0200

    Initialized Project

---
+++ src/test/java/fr/diegoimbert/cvman/CvmanApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diegoimbert.cvman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
