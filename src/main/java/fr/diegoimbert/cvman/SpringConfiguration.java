cad41bbcdf4d540263bbb5e88713585be5874d22
Diego Imbert <diegoimbert@pm.me>
2023-10-05 11:16:25 +0200

    SpringConfiguration

---
+++ src/main/java/fr/diegoimbert/cvman/SpringConfiguration.java
@@ -0,0 +1,54 @@
+package fr.diegoimbert.cvman;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringConfiguration extends SpringBootServletInitializer implements WebMvcConfigurer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Starter.class);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("--- addResourceHandlers");
+		registry.addResourceHandler("/webjars/**")//
+				.addResourceLocations("/webjars/");
+	}
+
+	@Bean("messageSource")
+	public MessageSource messageSource() {
+		var r = new ReloadableResourceBundleMessageSource();
+		r.setBasenames("classpath:messages");
+		return r;
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public LocalValidatorFactoryBean getValidator() {
+		var factory = new LocalValidatorFactoryBean();
+		factory.setValidationMessageSource(messageSource());
+		return factory;
+	}
+}
--- src/main/java/fr/diegoimbert/cvman/Starter.java
+++ src/main/java/fr/diegoimbert/cvman/Starter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CvmanApplication {
+public class Starter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CvmanApplication.class, args);
+		SpringApplication.run(Starter.class, args);
 	}
 
 }
