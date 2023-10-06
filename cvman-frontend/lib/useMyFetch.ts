e6586040d8ebb3c7a70a8e22b82f5d3a455b9d17
Diego Imbert <diegoimbert@pm.me>
2023-10-06 21:17:26 +0200

    useMyFetch return type safety

--- cvman-frontend/lib/useMyFetch.ts
+++ cvman-frontend/lib/useMyFetch.ts
@@ -1,15 +1,20 @@
-import { ZodSchema } from "zod"
+import { AsyncData } from "nuxt/app"
+import { ZodType, z } from "zod"
+import type { FetchError } from "ofetch"
 
-export const useMyFetch = async (
-  request: Parameters<typeof useFetch>[0],
-  { schema, ...opts }: Parameters<typeof useFetch>[1] & { schema?: ZodSchema } = {}
-) => {
-  const result = await useFetch(request, { baseURL: "http://localhost:8080/", ...opts })
+export async function useMyFetch<T, ErrorT = FetchError>(
+  request: string,
+  { schema, ...opts }: { schema: ZodType<T> } & Parameters<typeof useFetch<T, ErrorT>>[1]
+): Promise<AsyncData<T, ErrorT>> {
+  const result = await useFetch<T, ErrorT>(request, {
+    baseURL: "http://localhost:8080/",
+    ...opts
+  })
   return {
     ...result,
     data: {
       ...result.data,
-      value: schema?.parse(result.data.value) ?? result.data.value
+      value: schema.parse(result.data.value)
     }
   }
 }
